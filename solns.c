928615d70b17bf6b2f999954e77195eb4e914a71
ranjana1999 <52955034+ranjana1999@users.noreply.github.com>
2021-06-16 15:07:36 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -1,18 +1,5 @@
 /* Enter your solutions in this file */
 #include <stdio.h>
-#include <assert.h>
-
-
-int max(int [], int);
-
-int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert (max(x, 7) == 10);
-
-  int y[] = {5};
-  assert (max(y, 1) == 5);
-  printf("Max: passed\n");
-  }
 int max(int arr[],int n){
     int large=arr[0];
     for(int i=0;i<n;i++){
